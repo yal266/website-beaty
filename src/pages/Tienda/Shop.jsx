ce8a0fa9f684e9aa50b2049691c9d055e729948f
YaLi26 <yd861297@gmail.com>
2024-07-13 14:55:21 -0500

    se agrego el approutes

---
+++ src/pages/Tienda/Shop.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Shop = () => {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: "0",
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 5,
+          },
+        },
+      ],
+    };
+  return (
+    <div className="container mt-5">
+      <Slider {...settings}>
+        {ImagesCircle.map((image) => (
+          <div key={image.id} className="mx-auto">
+            <div
+              className="rounded-circle overflow-hidden d-flex align-items-center border border-white"
+              style={{ width: "75px", height: "75px" }}
+            >
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="img-fluid"
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
+              />
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+export default Shop
