8c63055bb5df40ed7277de5f76bee2e79cb08843
YaLi26 <yd861297@gmail.com>
2024-07-20 09:21:36 -0500

    se crearon dos endpoint hasta el momento categoria y productos

---
+++ src/utils/APIweb.jsx
@@ -0,0 +1,4 @@
+export const API_BASE_URL = "https://servicios.campus.pe/";
+
+export const PRODUCTS_ENDPOINT = "productos.php";
+export const CATEGORY_ENDPOINT = "categorias.php";
--- src/utils/index.jsx
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = "https://servicios.campus.pe";
-
-export const PRODUCTS_ENDPOINT = "/productos.php";
